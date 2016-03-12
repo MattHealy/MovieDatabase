3c372f9fb5b31b91d5ac0d66c395d49d76748f6a
Matt Healy <healmatt@gmail.com>
2016-03-12 14:45:35 +0800

    More progress
    Can now add items to library and order them.

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,8 +1,18 @@
 from flask import g, current_app
 from flask.ext.wtf import Form
-from wtforms import TextField, SubmitField
+from wtforms import TextField, SubmitField, SelectField
 from wtforms.validators import Required 
 
 class TitleSearchForm(Form):
     title = TextField('title', validators=[Required()])
     submit = SubmitField('Search')
+
+class AddEntryForm(Form):
+    submit = SubmitField('Add')
+
+class LibrarySortForm(Form):
+    order_by = SelectField('Order By', choices=[
+        ("title","Order by Title"),
+        ("titleDesc","Order by Title (desc)"),
+        ("timestamp","Order by Date Added"),
+        ("timestampDesc","Order by Date Added (desc)")])
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,13 +1,13 @@
 from flask import render_template, flash, redirect, url_for, request, g, current_app, session, \
-                  abort 
+                  abort, Markup
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from datetime import datetime, timedelta, date
 from itsdangerous import JSONWebSignatureSerializer
 from . import admin
 from .. import db, lm
-from ..models import User
+from ..models import User, Entry
 from ..email import send_email
-from .forms import TitleSearchForm
+from .forms import TitleSearchForm, AddEntryForm, LibrarySortForm
 import omdb
 
 @lm.user_loader
@@ -17,7 +17,7 @@ def load_user(id):
 @admin.before_request
 def before_request():
     g.user = current_user
-    if g.user.is_authenticated():
+    if g.user.is_authenticated:
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
@@ -38,9 +38,30 @@ def home():
     if not g.user.is_confirmed():
         return redirect(url_for('admin.unconfirmed'))
 
-    return render_template("admin/home.html", title="My Collection", subtitle="Home", movies=g.user.entries)
+    movies = Entry.query.filter_by(user_id = g.user.id)
 
-@admin.route('/add/', methods=['GET','POST'])
+    order_by = request.args.get('order_by', 'title')
+
+    sortform = LibrarySortForm()
+    sortform.order_by.data = order_by
+
+    if order_by == 'timestamp':
+        movies = movies.order_by(Entry.timestamp)
+    elif order_by == 'timestampDesc':
+        movies = movies.order_by(Entry.timestamp.desc())
+    elif order_by == 'year':
+        movies = movies.order_by(Entry.year)
+    elif order_by == 'yearDesc':
+        movies = movies.order_by(Entry.year.desc())
+    elif order_by == 'titleDesc':
+        movies = movies.order_by(Entry.title.desc())
+    else:
+        movies = movies.order_by(Entry.title)
+
+    return render_template("admin/home.html", title="My Collection", subtitle="Home", movies=movies,
+                           sortform=sortform)
+
+@admin.route('/entry/add/', methods=['GET','POST'])
 @login_required
 def add_entry():
 
@@ -48,6 +69,7 @@ def add_entry():
         return redirect(url_for('admin.unconfirmed'))
 
     form = TitleSearchForm()
+    addform = AddEntryForm()
     movies = []
 
     if request.args.get('title'):
@@ -56,8 +78,6 @@ def add_entry():
         res = omdb.search(request.args.get('title'))
 
         for item in res:
-            if item['type'] != 'movie':
-                continue
             movie = {}
             movie['title'] = item['title']
             movie['year'] = item['year']
@@ -66,7 +86,42 @@ def add_entry():
             movie['imdb_id'] = item['imdb_id']
             movies.append(movie)
 
-    return render_template("admin/add.html", form=form, title="Add Entry", subtitle="Search", movies=movies)
+    return render_template("admin/add.html", form=form, title="Add Entry", subtitle="Search", \
+                           movies=movies, addform = addform)
+
+@admin.route('/entry/add/<imdb_id>/', methods=['POST'])
+@login_required
+def submit_entry(imdb_id):
+
+    if not g.user.is_confirmed():
+        return redirect(url_for('admin.unconfirmed'))
+
+    res = omdb.imdbid(imdb_id)
+
+    entry = Entry.query.filter_by(user_id = g.user.id, imdb_id = imdb_id).first()
+
+    if entry:
+        flash('This entry already exists in your collection')
+        return redirect(url_for('admin.home'))
+
+    entry = Entry(user_id = g.user.id, timestamp = datetime.utcnow())
+
+    title = res['title']
+    entry.title = res['title']
+    entry.year = res['year']
+    if res['poster'] != 'N/A':
+        entry.image = res['poster']
+
+    entry.imdb_id = res['imdb_id']
+
+    #Item({'genre': 'Thriller', 'poster': 'http://ia.media-imdb.com/images/M/MV5BMTUwMDQxNjIxM15BMl5BanBnXkFtZTcwMzg5MzkwNA@@._V1_SX300.jpg', 'type': 'movie', 'imdb_votes': '202,075', 'year': '2007', 'runtime': '113 min', 'released': '31 May 2007', 'awards': '6 nominations.', 'actors': 'Kurt Russell, Zoë Bell, Rosario Dawson, Vanessa Ferlito', 'plot': 'Two separate sets of voluptuous women are stalked at different times by a scarred stuntman who uses his "death proof" cars to execute his murderous plans.', 'title': 'Death Proof', 'imdb_rating': '7.1', 'language': 'English', 'director': 'Quentin Tarantino', 'country': 'USA', 'response': 'True', 'metascore': 'N/A', 'imdb_id': 'tt1028528', 'rated': 'R', 'writer': 'Quentin Tarantino'})
+
+    db.session.add(entry)
+    db.session.commit()
+
+    flash(Markup('Entry added successfully. <strong><a href="' + url_for('admin.add_entry') + '">Add another?</a></strong>'))
+
+    return redirect(url_for('admin.home'))
 
 @admin.route('/confirmation_email/', methods=['GET'])
 @login_required
