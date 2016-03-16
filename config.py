fa7216968c6b9ec6b704fc6f5b1e4001e39b6c8e
Matt Healy <healmatt@gmail.com>
2016-03-16 12:56:31 +0800

    Use environment variable for redis connection details

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config:
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     CSRF_ENABLED = True
 
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
