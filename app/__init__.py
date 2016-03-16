0f787149992da205251a1c789f2542a0fc5e744f
Matt Healy <healmatt@gmail.com>
2016-03-16 12:25:05 +0800

    Remove logging

--- app/__init__.py
+++ app/__init__.py
@@ -52,16 +52,16 @@ def create_app(config_name):
 
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
-    if not app.debug:
+    #if not app.debug:
 
-        import logging
-        from logging.handlers import RotatingFileHandler, SMTPHandler
+    #    import logging
+    #    from logging.handlers import RotatingFileHandler, SMTPHandler
 
-        file_handler = RotatingFileHandler('tmp/app.log', 'a', 1 * 1024 * 1024, 10)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        app.logger.setLevel(logging.INFO)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-        app.logger.info('app startup')
+    #    file_handler = RotatingFileHandler('tmp/app.log', 'a', 1 * 1024 * 1024, 10)
+    #    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    #    app.logger.setLevel(logging.INFO)
+    #    file_handler.setLevel(logging.INFO)
+    #    app.logger.addHandler(file_handler)
+    #    app.logger.info('app startup')
 
     return app
